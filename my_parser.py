5b3fce2c1a04d94197a7a9d016349401bbe0e4f9
Aleksandr <56115945+AlSavIg@users.noreply.github.com+alesavigo@gmail.com>
2022-06-24 01:12:42 +0300

    Реализована функция parse_json_card, собирающая данные из полученной карточки товара в формате json

--- my_parser.py
+++ my_parser.py
@@ -13,7 +13,20 @@ selected_stores = {
 
 
 def parse_json_card(item):
-    pass
+    name = item.get('name')
+    img_link = item.get('img_link')
+    promo_date_begin = item.get('promo').get('date_begin')
+    promo_date_end = item.get('promo').get('date_end')
+    promo_date = f'{promo_date_begin} - {promo_date_end}'
+    old_price = item.get('current_prices').get('price_reg__min')
+    new_price = item.get('current_prices').get('price_promo__min')
+    discount = f'{round((1 - new_price / old_price) * 100)}%'
+    return [name,
+            img_link,
+            promo_date,
+            old_price,
+            new_price,
+            discount]
 
 
 def get_data_from_json(local_url):
