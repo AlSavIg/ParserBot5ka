bdcc6087181b1eb2fa4b7f7856d6521aa3238aaa
User <slava.vdovkin.18@mail.ru>
2022-06-25 20:57:33 +0300

    Генерирует клавиатуру с выбором магазина для пользователя

--- bot.py
+++ bot.py
@@ -1,6 +1,15 @@
-from aiogram import Dispatcher, Bot
+from aiogram import Dispatcher, Bot, types
+
+from ParserBot5ka.my_parser import selected_stores
 
 token = "5387713157:AAHc0bfR2F8m1WzPiCfPa4_ACi0K39VCZV4"
 bot = Bot(token=token)
 dp = Dispatcher(bot)
 
+
+@dp.message_handler(commands='start')
+async def start(message: types.Message):
+    start_buttons = [selected_stores.get(key) for key in selected_stores]
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*start_buttons)
+    await message.answer('Пожалуйста, выберите магазин', reply_markup=keyboard)
