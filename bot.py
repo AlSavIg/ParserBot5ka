23f6e53082903dfdfbe1c0136d953cce7cd99cc4
User <slava.vdovkin.18@mail.ru>
2022-06-25 21:01:17 +0300

    Ожидает выбор магазина пользователем

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 from aiogram import Dispatcher, Bot, types
+from aiogram.dispatcher.filters import Text
+from aiogram.utils import executor
 
 from ParserBot5ka.my_parser import selected_stores
 
@@ -13,3 +15,30 @@ async def start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*start_buttons)
     await message.answer('Пожалуйста, выберите магазин', reply_markup=keyboard)
+
+
+def send_message(message, shop_id):
+    pass
+
+
+@dp.message_handler(Text(equals=selected_stores.get('5677')))
+async def shop5677(message: types.Message):
+    await send_message(message=message, shop_id='5677')
+
+
+@dp.message_handler(Text(equals=selected_stores.get('33YU')))
+async def shop33YU(message: types.Message):
+    await send_message(message=message, shop_id='33YU')
+
+
+@dp.message_handler(Text(equals=selected_stores.get('5593')))
+async def shop5593(message: types.Message):
+    await send_message(message=message, shop_id='5593')
+
+
+def main():
+    executor.start_polling(dp)
+
+
+if __name__ == '__main__':
+    main()
